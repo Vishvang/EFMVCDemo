d5887b2f9d3b9c1250737bdf3fad71fa718b0184
Vishvang <vishvangp.8@gmail.com>
2019-04-22 21:44:32 -0400

    If we want to change Table Column or Foreign Key Name at the creation of Database and It's Table we can use DataAnnotations Schema Library. We can see that in Class Name can create table name as EmployeeViewModel which we dont want and we add that property to create table with appropriate class.

--- EFMVCDemo/Models/EmployeeViewModel.cs
+++ EFMVCDemo/Models/EmployeeViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace EFMVCDemo.Models
 {
+    [Table("Employee")]
     public class EmployeeViewModel
     {
         [Key]
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        //After creating table we have field Department_DepartmentId
+        //So create this property and pass this property to ForeignKey Property 
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
         public DepartmentViewModel Department { get; set; }
     }
 }
